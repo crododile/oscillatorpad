f2a4237075aaac569342c855301dc2d3c65b0d54
Coburn Berry <coburn.d.berry@gmail.com>
2014-05-01 18:09:29 -0700

    maybe added touches

--- public/javascripts/oscillator.js
+++ public/javascripts/oscillator.js
@@ -12,6 +12,29 @@ $(function(){
 	} else {
 	    throw new Error('AudioContext not supported. :(');
 	}
+	
+	function touchHandler(event)
+	{
+	    var touches = event.changedTouches,
+	        first = touches[0],
+	        type = "";
+	         switch(event.type)
+	    {
+	        case "touchstart": type = "mousedown"; break;
+	        case "touchmove":  type="mousemove"; break;        
+	        case "touchend":   type="mouseup"; break;
+	        default: return;
+	    }
+
+	    event.preventDefault();
+	}
+
+	
+	    document.addEventListener("touchstart", touchHandler, true);
+	    document.addEventListener("touchmove", touchHandler, true);
+	    document.addEventListener("touchend", touchHandler, true);
+	    document.addEventListener("touchcancel", touchHandler, true);    
+	
 		
 	oscillator = context.createOscillator();
 	
