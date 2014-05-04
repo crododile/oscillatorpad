6dbee13721eb7da251bf992270865a6f89a08156
Coburn Berry <coburn.d.berry@gmail.com>
2014-05-03 23:23:30 -0700

    triads fire, but dont flow

--- public/javascripts/oscillator.js
+++ public/javascripts/oscillator.js
@@ -3,8 +3,9 @@ $(function(){
 	var oscillator;
 	var volumeNode;
 	var wavetype = "sine";
+	var triad = false;
 
-	
+
 	if (typeof AudioContext !== "undefined") {
 	    context = new AudioContext();
 	} else if (typeof webkitAudioContext !== "undefined") {
@@ -15,7 +16,6 @@ $(function(){
 	
 	function touchHandler(event)
 	{
-
 	    var touches = event.changedTouches,
 	        first = touches[0],
 	        type = "";
@@ -29,13 +29,11 @@ $(function(){
 	    event.preventDefault();
 	}
 
-	
     $('.pad').on("touchstart", touchHandler);
     $('.pad').on("touchmove", touchHandler, true);
     $('.pad').on("touchend", touchHandler, true);
     $('.pad').on("touchcancel", touchHandler, true);    
-	
-		
+			
 	oscillator = context.createOscillator();
 	
 	volumeNode = context.createGainNode();
@@ -47,18 +45,53 @@ $(function(){
 	oscillator.start();
 	
 	$('button').on('click',function(e){
+		if (e.target.textContent === "CLEAR LAST NODE"){
+			 $('div.node').last().trigger('click');
+		 }
+		 if (e.target.textContent === "TRIAD"){
+			 triad = true;
+			e.stopPropogation();
+		 }
+		 
 		oscillator.type = e.target.textContent.toLowerCase()
 		wavetype = oscillator.type;
 	})
+
 	
-    $('.pad').on('mousedown', function(e) {
+	
+    $('.pad').on('mousedown', function placeNode(e) {
 		var d = new Date();
 		var start = d.getTime();
 		var traveller = $('<div id="traveller" class="node"></div>');
 		traveller.css({ top: e.clientY + $(document).scrollTop() -25, left: e.clientX-25 });
 		traveller.addClass(wavetype);
 		$('body').append(traveller);
+		
 		var datas = [];
+		
+		if (triad === true){
+			var thirdDatas = [];
+			var fifthDatas = [];
+			
+			var thirdOs = context.createOscillator();
+			var fifthOs = context.createOscillator();
+			
+			thirdOs.connect(volumeNode);
+			thirdOs.start();
+			
+			fifthOs.connect(volumeNode);
+			fifthOs.start();
+			
+			var third = $('<div class="node"></div>');
+			third.css({ top: e.clientY + $(document).scrollTop() -25, left: (e.clientX * 1.25999) -25 });
+			third.addClass(wavetype);
+			$('body').append(third);
+			
+			var fifth = $('<div class="node"></div>');
+			fifth.css({ top: e.clientY + $(document).scrollTop() -25, left: (e.clientX * 1.498307) -25 });
+			fifth.addClass(wavetype);
+			$('body').append(fifth);
+		}
 		
 		$('.pad').mousemove(function(e){
 
@@ -69,41 +102,67 @@ $(function(){
 			interval = now - start;
 
 			datas.push([e.clientX, e.clientY, interval, { top: e.clientY + $(document).scrollTop() -25, left: e.clientX-25 }]);
-			
-			data = {
-	            x: (e.clientX),
-	            y: (e.clientY),
-	            type: e.handleObj.type
-	        }
+	
 			
 	      traveller.css({ top: e.clientY + $(document).scrollTop() -25, left: e.clientX-25 });
+		  var pathNode = $("<div class='path'></div>");
+		  pathNode.addClass(wavetype);
+		  pathNode.css({ top: e.clientY + $(document).scrollTop(), left: e.clientX });
+		  $('body').append(pathNode);
 	
-		  volumeNode.gain.value = $('.pad').height()/data.y - 1;
-	  	  oscillator.frequency.value = data.x;
+		  volumeNode.gain.value = $('.pad').height()/e.clientY - 1;
+	  	  oscillator.frequency.value = e.clientX;
 		  traveller.html("<span style='position:relative; top: 20px'>" + oscillator.frequency.value + "Hz</span>");
+		  
+		if(triad === true){
+			thirdDatas.push([e.clientX *1.259999, e.clientY, interval, { top: e.clientY + $(document).scrollTop() -25, left: (e.clientX * 1.25999) -25 }]);
+   			third.css({ top: e.clientY + $(document).scrollTop() -25, left: (e.clientX * 1.25999) -25 });
+			thirdOs.frequency.value = e.clientX *1.25999;
+			third.html("<span style='position:relative; top: 20px'>" + Math.floor(thirdOs.frequency.value) + "Hz</span>");
+			
+			fifthDatas.push([e.clientX *1.498307, e.clientY, interval, { top: e.clientY + $(document).scrollTop() -25, left: (e.clientX * 1.498307) -25 }]);
+   			fifth.css({ top: e.clientY + $(document).scrollTop() -25, left: (e.clientX * 1.498307) -25 });
+			fifthOs.frequency.value = e.clientX *1.498307;
+			console.log(fifth, fifthOs)
+			fifth.html("<span style='position:relative; top: 20px'>" + Math.floor(fifthOs.frequency.value) + "Hz</span>");
+		}
 	  });
 		
 		$('#traveller').mousemove(function(e){
-
+			var x = e.clientX;
+			var y = e.clientY;
 			var d2 = new Date();
 			var now = d2.getTime();
 			var interval;
 		
 			interval = now - start;
 
-			datas.push([e.clientX, e.clientY, interval, { top: e.clientY + $(document).scrollTop() -25, left: e.clientX-25 }]);
+			datas.push([x, y, interval, { top: y + $(document).scrollTop() -25, left: x-25 }]);
 			
-			data = {
-	            x: (e.clientX),
-	            y: (e.clientY),
-	            type: e.handleObj.type
-	        }
-			
-	      traveller.css({ top: e.clientY + $(document).scrollTop() -25, left: e.clientX-25 });
-	
-		  volumeNode.gain.value = $('.pad').height()/data.y - 1;
-	  	  oscillator.frequency.value = data.x;
+	      traveller.css({ top: y + $(document).scrollTop() -25, left: x-25 });
+		  var pathNode = $("<div class='path'></div>");
+		  pathNode.addClass(wavetype);
+		  pathNode.css({ top: y + $(document).scrollTop(), left: x });
+		  $('body').append(pathNode);
+		  
+		  volumeNode.gain.value = $('.pad').height()/y - 1;
+	  	  oscillator.frequency.value = x;
 		  traveller.html("<span style='position:relative; top: 20px'>" + oscillator.frequency.value + "Hz</span>");
+	  
+		  
+  		if(triad === true){
+  			thirdDatas.push([e.clientX *1.259999, e.clientY, interval, { top: e.clientY + $(document).scrollTop() -25, left: (e.clientX * 1.25999) -25 }]);
+     		third.css({ top: e.clientY + $(document).scrollTop() -25, left: (e.clientX * 1.25999) -25 });
+  			thirdOs.frequency.value = e.clientX *1.25999;
+  			third.html("<span style='position:relative; top: 20px'>" + Math.floor(thirdOs.frequency.value) + "Hz</span>");
+			
+			fifthDatas.push([e.clientX *1.498307, e.clientY, interval, { top: e.clientY + $(document).scrollTop() -25, left: (e.clientX * 1.498307) -25 }]);
+   			fifth.css({ top: e.clientY + $(document).scrollTop() -25, left: (e.clientX * 1.498307) -25 });
+			fifthOs.frequency.value = e.clientX *1.498307;
+			fifth.html("<span style='position:relative; top: 20px'>" + Math.floor(fifthOs.frequency.value) + "Hz</span>");
+  		}
+		
+		
 	  });
 	  
 	  $('#traveller').on('mouseup', function(e){
@@ -138,6 +197,11 @@ $(function(){
 				copy.disconnect();
 				e.stopPropogation();
 		  	})
+			
+		  	$('div.path').on('click', function(e){
+			
+		  		  e.target.remove();
+		  	})
 		  
 		  newOs.start();
 		
@@ -161,13 +225,12 @@ $(function(){
 			  }, arr[2] - previousTimeout);
 		  }
 		  
+		  smoothly(datas[0], 0, 0)
+		  
 		  setInterval(function(){
 			  smoothly(datas[0], 0, 0);
-		  }, datas[datas.length-1][2]);
-		  
+		  }, datas[datas.length-1][2]); 
 	  });
-	  
-	  
 	});
 	
 	
